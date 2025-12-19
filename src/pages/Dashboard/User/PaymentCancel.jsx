e2d96cebeff7258bc5ae47f63e891e137e965009
mursalin35 <msmursalin35@gmail.com>
2025-12-19 12:26:18 +0600

    calcel payment btn update

--- src/pages/Dashboard/User/PaymentCancel.jsx
+++ src/pages/Dashboard/User/PaymentCancel.jsx
@@ -6,8 +6,8 @@ const PaymentCancel = () => {
         <div>
             <title>Payment Cancel</title>
             <h1>Payment is cancel. try again</h1>
-            <Link to="/dashboard/my-parcels">
-            <button className='btn btn-primary btn-sm text-black'>try again</button></Link>
+            <Link to="/dashboard/my-loans">
+            <button className='btn btn-primary px-4 btm btn-sm text-black'>try again</button></Link>
         </div>
     );
 };
