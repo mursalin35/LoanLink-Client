01441f8139f5eb606e6cc4950cccd160983d5d4d
mursalin35 <msmursalin35@gmail.com>
2025-12-31 17:03:11 +0600

    apply loan dark mode added

--- src/pages/Loans/ApplyLoan.jsx
+++ src/pages/Loans/ApplyLoan.jsx
@@ -81,158 +81,177 @@ const Application = () => {
     mutation.mutate(applicationData);
   };
 
-  if (isLoading) return <div className="text-center mt-10"><Spinner/></div>;
+  if (isLoading)
+    return (
+      <div className="text-center mt-10">
+        <Spinner />
+      </div>
+    );
 
   return (
-   <div className="max-w-4xl mx-auto px-4 py-10  min-h-screen">
-  <title>Apply Loan</title>
-  <h1 className="text-3xl font-bold mb-6 text-center text-[#1C2B27]">
-    Apply for {loan.title}
-  </h1>
+    <div className="max-w-4xl mx-auto px-6 py-12 min-h-screen ">
+      <title>Apply Loan</title>
 
-  <form
-    onSubmit={handleSubmit}
-    className="bg-[#FFFFFF] shadow-lg rounded-lg p-8 flex flex-col gap-4"
-  >
-    {/* Read-only auto fields */}
-    <input
-      type="email"
-      value={user.email}
-      readOnly
-      className="border  p-2 rounded bg-[#F4F7F5] text-[#1C2B27]"
-    />
-    <input
-      type="text"
-      value={loan.title}
-      readOnly
-      className="border  p-2 rounded bg-[#F4F7F5] text-[#1C2B27]"
-    />
-    <input
-      type="text"
-      value={loan.interest + "%"}
-      readOnly
-      className="border  p-2 rounded bg-[#F4F7F5] text-[#1C2B27]"
-    />
-    <input
-      type="text"
-      value={loan.category}
-      readOnly
-      className="border  p-2 rounded bg-[#F4F7F5] text-[#1C2B27]"
-    />
+      {/* Page Heading */}
+      <div className="mb-10 text-center text-[#1C2B27] dark:text-[#E6F4F1]">
+        <h1 className="text-3xl md:text-4xl font-extrabold">
+          Apply for {loan.title}
+        </h1>
 
-    {/* User input fields */}
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-      <input
-        type="text"
-        name="firstName"
-        placeholder="First Name"
-        value={formData.firstName}
-        onChange={handleChange}
-        required
-        className="border  p-2 rounded focus:outline-none focus:ring-2 focus:ring-[#B6E04C]"
-      />
-      <input
-        type="text"
-        name="lastName"
-        placeholder="Last Name"
-        value={formData.lastName}
-        onChange={handleChange}
-        required
-        className="border  p-2 rounded focus:outline-none focus:ring-2 focus:ring-[#B6E04C]"
-      />
+        <p className="text-center text-sm mt-4 md:text-base text-[#6B7C75] dark:text-[#A8C5BE] mb-10 max-w-2xl mx-auto">
+          Please fill in the form below to apply for this loan. Make sure all
+          information is accurate, as it will be reviewed by our verification
+          team.
+        </p>
+      </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="bg-[#FFFFFF] dark:bg-[#1C1C28] shadow-2xl rounded-3xl p-10 flex flex-col gap-6 transition-colors duration-300"
+      >
+        {/* Read-only auto fields */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <input
+            type="email"
+            value={user.email}
+            readOnly
+            className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg bg-[#F4F7F5] dark:bg-[#11121A] text-[#1C2B27] dark:text-[#E6F4F1] font-medium"
+          />
+          <input
+            type="text"
+            value={loan.title}
+            readOnly
+            className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg bg-[#F4F7F5] dark:bg-[#11121A] text-[#1C2B27] dark:text-[#E6F4F1] font-medium"
+          />
+          <input
+            type="text"
+            value={loan.interest + "%"}
+            readOnly
+            className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg bg-[#F4F7F5] dark:bg-[#11121A] text-[#1C2B27] dark:text-[#E6F4F1] font-medium"
+          />
+          <input
+            type="text"
+            value={loan.category}
+            readOnly
+            className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg bg-[#F4F7F5] dark:bg-[#11121A] text-[#1C2B27] dark:text-[#E6F4F1] font-medium"
+          />
+        </div>
+
+        {/* User input fields */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 text-[#1C2B27] dark:text-[#E6F4F1]">
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+            className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#B6E04C] dark:bg-[#11121A] "
+          />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+            className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#B6E04C] dark:bg-[#11121A] dark:text-[#E6F4F1]"
+          />
+        </div>
+
+        <input
+          type="number"
+          name="contactNumber"
+          placeholder="Contact Number"
+          value={formData.contactNumber}
+          onChange={handleChange}
+          required
+          className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#B6E04C] dark:bg-[#11121A]  text-[#1C2B27] dark:text-[#E6F4F1]"
+        />
+
+        <input
+          type="text"
+          name="nationalID"
+          placeholder="National ID / Passport Number"
+          value={formData.nationalID}
+          onChange={handleChange}
+          required
+          className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#B6E04C] dark:bg-[#11121A]  text-[#1C2B27] dark:text-[#E6F4F1]"
+        />
+
+        <input
+          type="text"
+          name="incomeSource"
+          placeholder="Income Source"
+          value={formData.incomeSource}
+          onChange={handleChange}
+          required
+          className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#B6E04C] dark:bg-[#11121A] text-[#1C2B27] dark:text-[#E6F4F1]"
+        />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          <input
+            type="number"
+            name="monthlyIncome"
+            placeholder="Monthly Income"
+            value={formData.monthlyIncome}
+            onChange={handleChange}
+            required
+            className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#B6E04C] dark:bg-[#11121A]  text-[#1C2B27] dark:text-[#E6F4F1]"
+          />
+          <input
+            type="number"
+            name="loanAmount"
+            placeholder="Loan Amount"
+            value={formData.loanAmount}
+            onChange={handleChange}
+            required
+            className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#B6E04C] dark:bg-[#11121A]  text-[#1C2B27] dark:text-[#E6F4F1]"
+          />
+        </div>
+
+        <input
+          type="text"
+          name="reason"
+          placeholder="Reason for Loan"
+          value={formData.reason}
+          onChange={handleChange}
+          required
+          className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#B6E04C] dark:bg-[#11121A]  text-[#1C2B27] dark:text-[#E6F4F1]"
+        />
+
+        <input
+          type="text"
+          name="address"
+          placeholder="Address"
+          value={formData.address}
+          onChange={handleChange}
+          required
+          className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#B6E04C] dark:bg-[#11121A]  text-[#1C2B27] dark:text-[#E6F4F1]"
+        />
+
+        <textarea
+          name="extraNotes"
+          placeholder="Extra Notes"
+          value={formData.extraNotes}
+          onChange={handleChange}
+          className="border border-gray-300 dark:border-gray-700 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#B6E04C] dark:bg-[#11121A]  text-[#1C2B27] dark:text-[#E6F4F1] resize-none h-28"
+        />
+
+        {/* Submit Button */}
+        <button
+          type="submit"
+          disabled={mutation.isLoading}
+          className={`${
+            mutation.isLoading
+              ? "bg-gray-400 cursor-not-allowed"
+              : "bg-[#387065] hover:bg-[#1F4F45]"
+          } text-white py-3 rounded-xl text-lg font-semibold transition-all duration-300 mt-4 shadow-lg hover:shadow-xl`}
+        >
+          {mutation.isLoading ? "Submitting..." : "Submit Application"}
+        </button>
+      </form>
     </div>
-
-    <input
-      type="number"
-      name="contactNumber"
-      placeholder="Contact Number"
-      value={formData.contactNumber}
-      onChange={handleChange}
-      required
-      className="border  p-2 rounded focus:outline-none focus:ring-2 focus:ring-[#B6E04C]"
-    />
-
-    <input
-      type="text"
-      name="nationalID"
-      placeholder="National ID / Passport Number"
-      value={formData.nationalID}
-      onChange={handleChange}
-      required
-      className="border  p-2 rounded focus:outline-none focus:ring-2 focus:ring-[#B6E04C]"
-    />
-
-    <input
-      type="text"
-      name="incomeSource"
-      placeholder="Income Source"
-      value={formData.incomeSource}
-      onChange={handleChange}
-      required
-      className="border  p-2 rounded focus:outline-none focus:ring-2 focus:ring-[#B6E04C]"
-    />
-
-    <input
-      type="number"
-      name="monthlyIncome"
-      placeholder="Monthly Income"
-      value={formData.monthlyIncome}
-      onChange={handleChange}
-      required
-      className="border  p-2 rounded focus:outline-none focus:ring-2 focus:ring-[#B6E04C]"
-    />
-
-    <input
-      type="number"
-      name="loanAmount"
-      placeholder="Loan Amount"
-      value={formData.loanAmount}
-      onChange={handleChange}
-      required
-      className="border  p-2 rounded focus:outline-none focus:ring-2 focus:ring-[#B6E04C]"
-    />
-
-    <input
-      type="text"
-      name="reason"
-      placeholder="Reason for Loan"
-      value={formData.reason}
-      onChange={handleChange}
-      required
-      className="border  p-2 rounded focus:outline-none focus:ring-2 focus:ring-[#B6E04C]"
-    />
-
-    <input
-      type="text"
-      name="address"
-      placeholder="Address"
-      value={formData.address}
-      onChange={handleChange}
-      required
-      className="border  p-2 rounded focus:outline-none focus:ring-2 focus:ring-[#B6E04C]"
-    />
-
-    <textarea
-      name="extraNotes"
-      placeholder="Extra Notes"
-      value={formData.extraNotes}
-      onChange={handleChange}
-      className="border  p-2 rounded focus:outline-none focus:ring-2 focus:ring-[#B6E04C]"
-    />
-
-    <button
-      type="submit"
-      disabled={mutation.isLoading}
-      className={`${
-        mutation.isLoading
-          ? "bg-gray-400 cursor-not-allowed"
-          : "bg-[#387065] hover:bg-[#1F4F45]"
-      } text-white py-3 rounded transition-all duration-300 mt-4`}
-    >
-      {mutation.isLoading ? "Submitting..." : "Submit Application"}
-    </button>
-  </form>
-</div>
   );
 };
 
